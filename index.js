997478accc077fe5047d0452b76370b818f6db88
Oksana Cook <oksanacook@outlook.com>
2022-03-15 10:46:45 +1100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+function add(a, b){
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+function divide(a, b) {
+    return (a / b);
+}
+
+
+function increment(n) {
+    return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+
+function makeInt(n){
+    var s = n.toString();
+    var l = s.length;
+    var x =  parseInt ( 'n', 10);
+  if (s[0]=== '0' && s[1]==='x' || s[1]==="X") {
+     return 0;
+   } else if (x === isNaN('x')){
+     return NaN;
+   } else {
+   return n;
+   }
+  }
+
+
+  function preserveDecimal(n){
+    return parseFloat(n);
+    }
